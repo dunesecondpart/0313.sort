3e16baf841e69c5557d6136185ad1637782b8f32
Munir <dunepart2@moonear.ca>
2024-03-13 17:42:30 -0600

    end of class

---
+++ index.js
@@ -0,0 +1,23 @@
+listOfCamels = [ 8, 2, 4, 1, 3];
+
+// sort that list of numbers
+// 1, 2, 3, 4, 8
+function SwapValues(a, b) {
+    if (a > b) {
+        let tmp = a;
+        b = a;
+        a = tmp;
+    }
+}
+
+function sortMe(myList) {
+    typeof(myList)
+    // while there is a swap of number, keep looping
+    isSorted = true;
+    isSwapDone = true;
+    indexCounter = 0;
+    while (isSwapDone) {
+
+    }
+
+}
